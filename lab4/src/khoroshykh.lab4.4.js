7977b44b4f6c2554308129d06e11f06ce64c31b8
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-07-31 11:18:35 +0300

    lab4

---
+++ lab4/src/khoroshykh.lab4.4.js
@@ -0,0 +1,25 @@
+'use strict';
+const x = 10;
+const y = 3;
+const action = "^";
+
+console.log(`${x} ${action} ${y} = ${calc(x, y, action)}`);
+
+function calc(x, y, action) {
+  switch (action) {
+    case "+":
+      return x + y;
+    case "-":
+      return x - y;
+    case "*":
+      return x * y;
+    case "/":
+      return x / y;
+    case "%":
+      return x % y;
+    case "^":
+      return x ^ y;
+    default:
+      return;
+  }
+}
