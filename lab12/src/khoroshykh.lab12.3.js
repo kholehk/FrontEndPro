d6e3f25dea277c1ace7424e888e971ac79f337ac
OPEX <olkhoroshykh@gmail.com>
2020-09-05 00:42:18 +0300

    DOM.event ver.3.0

--- lab12/src/khoroshykh.lab12.3.js
+++ lab12/src/khoroshykh.lab12.3.js
@@ -1,19 +1,40 @@
 "use strict";
 
 const RED = "rgb(255, 0, 0)";
-
-const menu = document.querySelectorAll(".menu_item");
+const items = ["File", "Edit", "Run", "Help"];
+const footer = document.querySelector("footer .container");
+const menu = createMenu({ parent: footer, items: items });
 
 for (let item of menu) {
   item.addEventListener("click", clickMenu);
 }
 
-// console.log(menu);
-
 function clickMenu(event) {
+  const menu = event.target.parentNode.childNodes;
+
   for (let item of menu) {
     item.style.backgroundColor = "";
   }
 
   event.target.style.backgroundColor = RED;
+}
+
+function createMenu(props) {
+  const items = [];
+  let itemElement;
+
+  const menu = document.createElement("ul");
+  menu.className = "menu";
+  props.parent.appendChild(menu);
+
+  for (let itemText of props.items) {
+    itemElement = document.createElement("li");
+    itemElement.innerText = itemText;
+    itemElement.className = "menu_item";
+
+    menu.appendChild(itemElement);
+    items.push(itemElement);
+  }
+
+  return items;
 }
