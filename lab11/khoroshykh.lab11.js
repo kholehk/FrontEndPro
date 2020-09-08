88c131cbb96b6ba0e0572c5148da3906fde1354a
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-09-09 01:10:04 +0300

    editUser delUser

--- lab11/khoroshykh.lab11.js
+++ lab11/khoroshykh.lab11.js
@@ -1,47 +1,47 @@
 "use strict";
 
-const itemButtons = [
+const DATANAME = "name";
+const userButtons = [
    {
-      text: "Edit",
+      text: "Редагувати",
       func: editUser,
    },
    {
-      text: "Del",
+      text: "Видалити",
       func: delUser,
    }
 ];
 
 const container = document.querySelector("main .container");
+const listUsers = document.querySelector("ol.users");
 
-if (container) { 
+if (container && listUsers) {
+   
+   listUsers.addEventListener("click", changeUser);
 
-   const formAddName = document.createElement("form");
-   formAddName.name = "addname";
-   container.appendChild(formAddName);
-   formAddName.addEventListener("submit", addName);
+   const formAddUser = document.createElement("form");
+   formAddUser.name = "addUser";
+   container.prepend(formAddUser);
+   formAddUser.addEventListener("submit", addUser);
 
-   const labelName = document.createElement("label");
-   labelName.innerHTML = "<em>Name:</em>";
-   formAddName.appendChild(labelName);
+   const labelAddUser = document.createElement("label");
+   labelAddUser.innerHTML = "<em>Користувач:</em>";
+   formAddUser.appendChild(labelAddUser);
 
-   const inputName = document.createElement("input");
-   inputName.placeholder = "Input Name";
-   inputName.name = "username";
-   inputName.style.margin = "10px";
-   labelName.appendChild(inputName);
+   const inputAddUser = document.createElement("input");
+   inputAddUser.type = "text";
+   inputAddUser.placeholder = "Ім'я";
+   inputAddUser.dataset.info = DATANAME;
+   inputAddUser.style.margin = "10px";
+   labelAddUser.appendChild(inputAddUser);
 
-   const buttonAddName = document.createElement("button");
-   buttonAddName.innerText = "Add";
-   buttonAddName.type = "submit";
-   formAddName.appendChild(buttonAddName);
-
-   const listUsers = document.createElement("ol");
-   listUsers.dataset.list = "users";
-   listUsers.style.margin = "10px";
-   container.appendChild(listUsers);
+   const buttonAddUser = document.createElement("button");
+   buttonAddUser.innerText = "Додати";
+   buttonAddUser.type = "submit";
+   formAddUser.appendChild(buttonAddUser);
 }
 
-function addName(event) { 
+function addUser(event) { //submit
    event.preventDefault();
 
    const inputName = Array
@@ -50,43 +50,45 @@ function addName(event) {
    
    if (!inputName.value) return;
 
-   const listUsersItem = document.createElement("li");
-   listUsersItem.innerText = inputName.value;
+   const userItem = document.createElement("li");
+   listUsers.appendChild(userItem);
 
-   const listUsers = Array
-      .from(document.querySelectorAll("ol"))
-      .find(elem => elem.dataset.list === "users");
-   listUsers.appendChild(listUsersItem);
-   listUsers.addEventListener("click", changeItem);
+   const userName = document.createElement("p");
+   userName.innerText = inputName.value;
+   inputName.value = "";
+   userName.dataset.info = DATANAME;
+   userItem.appendChild(userName);
 
-   itemButtons.forEach(elem =>
-      createButtonForItem({ text: elem.text, func: elem.func.name, item: listUsersItem })
+   userButtons.forEach(elem =>
+      createUserButton({ text: elem.text, func: elem.func.name, item: userItem })
    );
 }
 
 function isInputName(elem) {
-   return elem instanceof HTMLInputElement && elem.name === "username";
+   return elem instanceof HTMLInputElement && elem.dataset.info === DATANAME;
 }
 
-function createButtonForItem(props) {
+function createUserButton(props) {
    const button = document.createElement("button");
+   button.style.marginLeft = "5px";
    button.innerText = props.text;
+   button.type = "button";
    button.dataset.func = props.func;
-   button.style.margin = "10px";
    props.item.appendChild(button);
 }
 
-function changeItem(event) {
-   const ourItem = event.target.parentNode;
+function changeUser(event) {
+   const userItem = event.target.parentNode;
    const func = event.target.dataset.func;
    if (!func) return;
-   window[func](ourItem);
+   window[func](userItem);
 }
 
 function editUser(user) {
-   prompt("Edit User name:", user.innerHTML);
+   const userName = user.firstChild.innerText;
+   user.firstChild.innerText = prompt("Редагувати ім'я користувача:", userName) || userName;
 }
 
 function delUser(user) {
-   confirm("Is User delete?");
+   if (confirm("Видалити користувача?")) user.remove();
 }
