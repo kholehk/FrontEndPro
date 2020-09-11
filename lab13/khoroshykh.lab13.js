dda5e2795f7f1a66725bc32d781f7d9b30aa6873
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-09-12 00:46:42 +0300

    create Elements

--- lab13/khoroshykh.lab13.js
+++ lab13/khoroshykh.lab13.js
@@ -1,1 +1,76 @@
 "use strict";
+
+const container = document.querySelector("main .container");
+
+if (container !== null) { 
+   const formLogin = document.createElement("form");
+   formLogin.classList.add("login");
+   formLogin.name = "login";
+   container.appendChild(formLogin);
+
+   const labelEmail = createLabel(
+      { text: "Input Email: ", parent: formLogin }
+   );
+
+   const inputEmail = createInput(
+      {
+         type: "text",
+         placeholder: "Your Email",
+         info: "email", 
+         parent: labelEmail,
+      }
+   );
+
+   const labelPassword = createLabel(
+      { text: "Input password: ", parent: formLogin }
+   );
+
+   const inputPassword = createInput(
+      {
+         type: "password",
+         info: "passw",
+         parent: labelPassword,
+      }
+   );
+
+   const buttonSubmit = createButton(
+      {
+         type: "submit",
+         text: "Login",
+         parent: formLogin,
+      }
+   );
+}
+
+function createLabel(props) { 
+
+   const label = document.createElement("label");
+   label.innerText = props.text || "";
+   props.parent.appendChild(label);
+
+   return label;
+}
+
+function createInput(props) { 
+
+   const input = document.createElement("input");
+   if (props.type)
+      input.type = props.type;
+   if (props.placeholder)
+      input.placeholder = props.placeholder;
+   if (props.info)
+      input.dataset.info = props.info;
+   props.parent.appendChild(input);
+
+   return input;
+}
+
+function createButton(props) { 
+   const button = document.createElement("button");
+   if (props.type)
+      button.type = props.type;
+   button.innerText = props.text || "";
+   props.parent.appendChild(button);
+
+   return button;
+}
--- lab2/khoroshykh.lab2.6.js
+++
@@ -1,4 +0,0 @@
-const a = 154;
-let b = a % 10;
-
-console.log(`${b}` + (b % 2 ? `: odd` : `: even`));
