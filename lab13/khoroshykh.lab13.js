5eaa618f0915f663ae44c9c7889d95d5408dd6bf
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-09-20 01:11:21 +0300

    lab13 v1.0

--- lab13/khoroshykh.lab13.js
+++ lab13/khoroshykh.lab13.js
@@ -1,17 +1,25 @@
 "use strict";
 
+const _EMAIL = "email";
+const _PASSW = "password";
 const container = document.querySelector("main .container");
 
 if (container !== null) { 
 
-   let validEmail = false;
-   let validPassword = false;
+   // let validEmail = false;
+   // let validPassword = false;
+
+   const validForm = {
+      [_EMAIL]: false,
+      [_PASSW]: false
+   }
 
    const formLogin = document.createElement("form");
    formLogin.classList.add("login");
    formLogin.name = "login";
    container.appendChild(formLogin);
    formLogin.addEventListener("submit", submitLogin);
+   formLogin.addEventListener("change", changeInput);
 
    const labelEmail = createLabel(
       { text: "Input Email: ", parent: formLogin }
@@ -21,11 +29,11 @@ if (container !== null) {
       {
          type: "text",
          placeholder: "Your Email",
-         info: "email", 
+         key: _EMAIL, 
          parent: labelEmail,
       }
    );
-   inputEmail.addEventListener("change", changeEmail);
+   // inputEmail.addEventListener("change", changeEmail);
 
    const labelPassword = createLabel(
       { text: "Input password: ", parent: formLogin }
@@ -35,11 +43,11 @@ if (container !== null) {
    const inputPassword = createInput(
       {
          type: "password",
-         info: "passw",
+         key: _PASSW,
          parent: labelPassword,
       }
    );
-   inputPassword.addEventListener("change", changePassword);
+   // inputPassword.addEventListener("change", changePassword);
 
    const buttonPassword = createButton(
       {
@@ -60,31 +68,63 @@ if (container !== null) {
       }
    );
 
-   function changeEmail(event) {
-      validEmail = true;
-      inputEmail.style.border = "";
-      if (event.target.value === "") {
-         validEmail = false;
-         inputEmail.style.border = "2px solid red";
-      }
-      changeButtonSubmit();
+   // function changeEmail(event) {
+
+   //    validEmail = true;
+   //    inputEmail.style.border = "";
+
+   //    const value = event.target.value;
+   //    if (!/^([-.\w]+@([\w-]+\.)+[\w-]+)$/.test(value)) {
+   //       validEmail = false;
+   //       inputEmail.style.border = value !== "" ? "2px solid red" : "";
+   //    }
+
+   //    changeButtonSubmit();
+   // }
+
+   // function changePassword(event) {
+
+   //    validPassword = true;
+   //    inputPassword.style.border = "";
+
+   //    const value = event.target.value;
+   //    if (!/\d/.test(value) || !/^([@$#!?&\w]{8,})$/.test(value)) {
+   //       validPassword = false;
+   //       inputPassword.style.border = value !== "" ? "2px solid red" : "";
+   //    }
+
+   //    changeButtonSubmit();
+   // }
+
+   function changeInput(event) {
+
+      const key = event.target.dataset.key;
+      if (!key) return;
+
+      const value = event.target.value;
+
+      validForm[key] = isValidInput({ key, value });
+      event.target.style.border = (validForm[key] || value === "") ? "" : "2px solid red";
+
+      buttonSubmit.disabled = !(validForm[_EMAIL] && validForm[_PASSW]);
    }
 
-   function changePassword(event) {
-      validPassword = true;
-      inputPassword.style.border = "";
-      if (event.target.value === "") {
-         validPassword = false;
-         inputPassword.style.border = "2px solid red";
+   function isValidInput(props) { 
+      switch (props.key) {
+         case _EMAIL:
+            return /^([-.\w]+@([\w-]+\.)+[\w-]+)$/.test(props.value);
+         case _PASSW:
+            return /\d/.test(props.value) && /^([@$#!?&\w]{8,})$/.test(props.value);
+         default:
+            return false;
       }
-      changeButtonSubmit();
    }
 
-   function changeButtonSubmit () { 
-      buttonSubmit.disabled = true;
-      if (validEmail && validPassword)
-         buttonSubmit.disabled = false;
-   }
+   // function changeButtonSubmit() {
+   //    buttonSubmit.disabled = true;
+   //    if (valid[_EMAIL] && valid[_PASSW])
+   //       buttonSubmit.disabled = false;
+   // }
 
    function changeVisibilityPass(event) {
       inputPassword.type = inputPassword.type === "password" ? "text" : "password";
@@ -94,22 +134,25 @@ if (container !== null) {
    function submitLogin(event) {
       event.preventDefault();
       const login = {
-         email: inputEmail.value,
-         password: inputPassword.value
+         [_EMAIL]: inputEmail.value,
+         [_PASSW]: inputPassword.value
       };
       inputEmail.value = "";
       inputPassword.value = "";
       buttonSubmit.disabled = true;
 
-      console.log(`Email: ${ login.email }\nPassword: ${ login.password }`);
+      console.log(login);
    }
 }
 
 function createLabel(props) { 
 
    const label = document.createElement("label");
-   label.innerText = props.text || "";
-   props.parent.appendChild(label);
+   
+   if (props) { 
+      label.innerText = props.text || "";
+      props.parent.appendChild(label);
+   }
 
    return label;
 }
@@ -117,22 +160,27 @@ function createLabel(props) {
 function createInput(props) { 
 
    const input = document.createElement("input");
-   input.type = props.type;
-   if (props.placeholder)
-      input.placeholder = props.placeholder;
-   if (props.info)
-      input.dataset.info = props.info;
-   props.parent.appendChild(input);
+
+   if (props) {
+      input.type = props.type;
+      input.placeholder = props.placeholder || "";
+      input.dataset.key = props.key ||"";
+      props.parent.appendChild(input);
+   }
 
    return input;
 }
 
 function createButton(props) { 
+
    const button = document.createElement("button");
-   button.type = props.type;
-   button.innerText = props.text || "";
-   button.disabled = props.disabled;
-   props.parent.appendChild(button);
+
+   if (props) {
+      button.type = props.type;
+      button.innerText = props.text || "";
+      button.disabled = props.disabled || false;
+      props.parent.appendChild(button);
+   }
 
    return button;
 }
