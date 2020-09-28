810dd73ffd6ba0e44dbcc73e5e73f56ea0b19e8c
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-09-28 23:37:35 +0300

    formatTime

--- lab15/khoroshykh.lab15.js
+++ lab15/khoroshykh.lab15.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const container = document.querySelector(".container");
+
+
+
+const timerID = window.setInterval(
+   () => {
+      console.clear();
+      console.log(formatTime(new Date()));
+   },
+   1000
+);
+
+function formatTime(date, format) {
+   const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+   const min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+   const sec = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+   return `${hour}:${min}:${sec}`;
+}
