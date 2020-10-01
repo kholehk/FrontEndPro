e305599719b875b4ace8c706dd7b0b169b710446
OPEX <olkhoroshykh@gmail.com>
2020-10-01 23:15:48 +0300

    renderClock

--- lab15/khoroshykh.lab15.js
+++ lab15/khoroshykh.lab15.js
@@ -1,19 +1,38 @@
 "use strict";
 
+const formatTime = "HH:mm:ss";
 const container = document.querySelector(".container");
 
-const timerID = window.setInterval(
-   () => {
-      console.clear();
-      console.log(formatTime(new Date(), "HH:mm:ss"));
-   },
-   1000
-);
+renderClock(formatTime);
 
-function formatTime(date, format) {
-   const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
-   const min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-   const sec = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+const timerID = window.setInterval(() => {
+  const time = getTime(new Date(), formatTime);
 
-   return format.replace(/HH(.)mm(.)ss/, `${hour}$1${min}$2${sec}`);
+  console.clear();
+  console.log(time);
+  putTimeOnClock(time);
+}, 1000);
+
+function getTime(date, format) {
+  const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  const min =
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  const sec =
+    date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+  return format.replace(/HH(\D)mm(\D)ss/, `${hour}$1${min}$2${sec}`);
 }
+
+function renderClock(format) {
+  const clock = document.createElement("div");
+  clock.classList.add("clock");
+  container.appendChild(clock);
+
+  for (let char of format) {
+    let elementOfClock = document.createElement("div");
+    elementOfClock.innerText = char;
+    clock.appendChild(elementOfClock);
+  }
+}
+
+function putTimeOnClock(time) {}
