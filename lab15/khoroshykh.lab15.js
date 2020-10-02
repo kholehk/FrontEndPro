04953da6c9835d9f9b4d4272e569875322d31464
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-10-02 10:46:43 +0300

    on check

--- lab15/khoroshykh.lab15.js
+++ lab15/khoroshykh.lab15.js
@@ -9,40 +9,40 @@ container.appendChild(clock);
 renderClock(formatTime);
 
 const timerID = window.setInterval(() => {
-  const time = getTime(new Date(), formatTime);
+   const time = getTime(new Date(), formatTime);
 
-  console.clear();
-  console.log(time);
-  changeTimeOnClock(time);
+   console.clear();
+   console.log(time);
+   changeTimeOnClock(time);
 }, 1000);
 
 function getTime(date, format) {
-  const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+   const hour =
+      date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
 
-  const min =
-    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+   const min =
+      date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
 
-  const sec =
-    date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+   const sec =
+      date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
 
-  return format.replace(/HH(\D)mm(\D)ss/, `${hour}$1${min}$2${sec}`);
+   return format.replace(/HH(\D)mm(\D)ss/, `${hour}$1${min}$2${sec}`);
 }
 
 function renderClock(format) {
-  for (let char of format) {
-    clock.appendChild(document.createElement("div"));
-  }
+   for (let char of format) {
+      clock.appendChild(document.createElement("div"));
+   }
 }
 
 function changeTimeOnClock(time) {
-  for (let i = 0; i < clock.childNodes.length; i++) {
-    clock.childNodes[i].innerText = time[i];
-    clock.childNodes[
-      i
-    ].style.color = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
-  }
+   for (let i = 0; i < clock.childNodes.length; i++) {
+      clock.childNodes[i].innerText = time[i];
+      clock.childNodes[i].style.color =
+         `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+   }
 }
 
 function randomColor() {
-  return Math.round((Math.random() * 1000) % 256);
+  return Math.round(Math.random() * 1000 % 256);
 }
