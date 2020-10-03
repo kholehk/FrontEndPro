56d97450877d33733b1e1cb8b60203c6d3f42a5e
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-10-03 23:46:19 +0300

    getTime

--- lab16/khoroshykh.lab16.2.js
+++ lab16/khoroshykh.lab16.2.js
@@ -6,6 +6,32 @@ class Clock {
    constructor() {
       this.format = LIST_FORMATS[0];
    }
+   
+   render(format) {
+      this.format = format;
+      clock.innerHTML = "";
+      
+      for (let i = 0; format[i] !== '/' && i < format.length; i++) {
+         clock.appendChild(document.createElement("div"));
+      }
+
+      if (+format.split("/")[1] === 12) { 
+         clock.appendChild(document.createElement("div"));
+      }
+   }
+   
+   getTime(date, format) {
+      const hour =
+         date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+
+      const min =
+         date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+
+      const sec =
+         date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+      return format.replace(/HH(\D)mm(\D)ss/, `${hour}$1${min}$2${sec}`);
+   }
 }
 const myClock = new Clock();
 const container = document.querySelector(".container");
@@ -19,7 +45,7 @@ LIST_FORMATS.forEach(element => {
    option.text = element;
    select.appendChild(option);
 });
-select.addEventListener("change", event => renderClock(event.target.value));
+select.addEventListener("change", event => myClock.render(event.target.value));
 
 label.appendChild(select);
 container.appendChild(label);
@@ -27,36 +53,15 @@ container.appendChild(label);
 const clock = document.createElement("div");
 clock.classList.add("clock");
 container.appendChild(clock);
-renderClock(myClock.format);
+myClock.render(myClock.format);
 
 const timerID = window.setInterval(() => {
-   const time = getTime(new Date(), myClock.format);
+   const time = myClock.getTime(new Date(), myClock.format);
 
    // console.clear();
    // console.log(time);
    changeTimeOnClock(time);
 }, 1000);
-
-function getTime(date, format) {
-   const hour =
-      date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
-
-   const min =
-      date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-
-   const sec =
-      date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
-
-   return format.replace(/HH(\D)mm(\D)ss/, `${hour}$1${min}$2${sec}`);
-}
-
-function renderClock(format) {
-   myClock.format = format;
-   clock.innerHTML = "";
-   for (let char of format) {
-      clock.appendChild(document.createElement("div"));
-   }
-}
 
 function changeTimeOnClock(time) {
    for (let i = 0; i < clock.childNodes.length; i++) {
