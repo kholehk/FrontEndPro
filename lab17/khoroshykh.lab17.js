18b66ecc34e66f1e35b0555ccef3cb803d4288e7
kholehk <66831553+kholehk@users.noreply.github.com>
2020-10-06 11:42:38 +0300

    delItem

--- lab17/khoroshykh.lab17.js
+++ lab17/khoroshykh.lab17.js
@@ -1,19 +1,53 @@
 "use strict";
 
+class List {
+   constructor(list) {
+      this.element = document.createElement("ul");
+      list.forEach(item => this.addItem(item));
+      this.element.addEventListener("click", event => console.log(event.target.dataset.button));
+   }
+
+   addItem(text) {
+      const li = document.createElement("li");
+      li.innerText = text;
+
+      const buttonDel = document.createElement("button");
+      buttonDel.innerText = "Del";
+      buttonDel.style.marginLeft = "5px";
+      buttonDel.dataset.button = "delItem";
+
+      this.element.appendChild(li);
+      li.appendChild(buttonDel);
+   }
+
+   delItem(li) { 
+      this.element.delete(li);
+   }
+}
+
 const container = document.querySelector(".container");
+
+const list = new List([]);
 
 const label = document.createElement("label");
 label.innerText = "Input something: ";
 label.classList.add("add_something");
 
 const input = document.createElement("input");
+input.style.marginLeft = "5px";
 
 const buttonAdd = document.createElement("button");
 buttonAdd.innerText = "Add";
-
-const list = document.createElement("ul");
+buttonAdd.addEventListener("click", event =>
+   {
+      const item = event.target.previousSibling.value;
+      console.log(item);
+      list.addItem(item);
+   }
+);
 
 container.appendChild(label);
-container.appendChild(list);
 label.appendChild(input);
 label.appendChild(buttonAdd);
+
+container.appendChild(list.element);
