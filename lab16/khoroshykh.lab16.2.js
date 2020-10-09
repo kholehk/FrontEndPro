84d9518e3fa614c6f13c1dd970ce95020d27958f
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-10-09 23:16:48 +0300

    renderClock

--- lab16/khoroshykh.lab16.2.js
+++ lab16/khoroshykh.lab16.2.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const LIST_FORMATS = ["HH:mm:ss", "HH:mm:ss/12", "mm:ss", "ss", "HH:ss/12"];
-const amountClocksDiv = 10;
 let clockFormat = LIST_FORMATS[0];
 
 const container = document.querySelector(".container");
@@ -15,16 +14,17 @@ LIST_FORMATS.forEach(element => {
    option.text = element;
    select.appendChild(option);
 });
-select.addEventListener("change", event => clockFormat = event.target.value);
+select.addEventListener("change", event => {
+   clockFormat = event.target.value;
+   renderClock(getTime(new Date()));
+});
 
 label.appendChild(select);
 container.appendChild(label);
 
 const clock = document.createElement("div");
 clock.classList.add("clock");
-for (let i = 0; i < amountClocksDiv; i++) { 
-   clock.appendChild(document.createElement("div"));
-}
+renderClock(getTime(new Date()));
 container.appendChild(clock);
 
 const timerID = window.setInterval(() => {
@@ -37,17 +37,17 @@ const timerID = window.setInterval(() => {
 
 function getTime(date) {
    const ampm = +clockFormat.split("/")[1] || 24;
-   let hour = +date.getHours();
+   let hour = date.getHours();
    const tail = ampm - 12 ? "" : (hour < 12 ? "AM" : "PM");
    hour = hour >= ampm ? hour - ampm : hour;
    hour = hour < 10 ? "0" + hour : hour; 
    hour = /HH/.test(clockFormat) ? hour : "__";
 
-   let min = +date.getMinutes();
+   let min = date.getMinutes();
    min = min < 10 ? "0" + min : min;
    min = /mm/.test(clockFormat) ? min : "__";
 
-   let sec = +date.getSeconds();
+   let sec = date.getSeconds();
    sec = sec < 10 ? "0" + sec : sec;
    sec = /ss/.test(clockFormat) ? sec : "__";
 
@@ -55,13 +55,20 @@ function getTime(date) {
 }
 
 function changeTimeOnClock(time) {
-   for (let i = 0; i < clock.childNodes.length; i++) {
+   for (let i = 0; i < time.length; i++) {
       clock.childNodes[i].innerText = time[i] || "";
       clock.childNodes[i].style.color =
          `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
    }
 }
 
+function renderClock(time) {
+   clock.innerHTML = "";
+   for (let i = 0; i < time.length; i++) { 
+      clock.appendChild(document.createElement("div"));
+   }
+}
+ 
 function randomColor() {
    return Math.round(Math.random() * 1000 % 256);
 }
