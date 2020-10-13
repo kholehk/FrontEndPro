65fdbab6fc0a497225609404a6aa2abfc6fdd3de
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-10-13 10:23:09 +0300

    render

--- lab18/khoroshykh.lab18.js
+++ lab18/khoroshykh.lab18.js
@@ -3,29 +3,40 @@
 class RequestList { 
    constructor(props) { 
       if (!props || !props.url) return;
-      this.url = new URL(props.url);
-      console.log(this.characters);
-   }
 
-   get characters() { 
+      this.url = new URL(props.url);
+      this.url.searchParams.set("page", "1");
+
       this.xhr = new XMLHttpRequest();
+      this.xhr.timeout = 10000;
       this.xhr.open("GET", this.url);
-      this.xhr.responseType = "json";
       this.xhr.send();
+
       this.xhr.onload = function () {
          if (this.xhr.status !== 200) {
             console.error(`${this.xhr.status}: ${this.xhr.statusText}`);
          } else {
-            try { 
-               return JSON.parse(this.xhr.responce);
-            } catch (err) { 
-               console.error(err);
-            }
+            this.render();
          }
       }.bind(this);
+
       this.xhr.onerror = function () {
          console.error("Request is failed!");
       }
+   }
+
+   get names() {
+      try {
+         return JSON.parse(this.xhr.response).results.map(elem => elem.name);
+      }
+      catch (err) {
+         console.error(err);
+         return [];
+      }
+   }
+
+   render() { 
+      console.log(this.names);
    }
 }
 
