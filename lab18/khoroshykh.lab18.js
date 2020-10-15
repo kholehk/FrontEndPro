15a7c183d4d10f0ef6e3746c3821dd09c80f6cdf
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-10-15 10:18:29 +0300

    init, render

--- lab18/khoroshykh.lab18.js
+++ lab18/khoroshykh.lab18.js
@@ -3,46 +3,75 @@
 class RequestList { 
    constructor(props) { 
       if (!props || !props.url) return;
-
       this._url = new URL(props.url);
+      //this._url.searchParams.set("page", 1);
 
-      this.xhr(1);
-      this.render();
+      let container = document.querySelector(props.container);
+      container = container ? container : document.querySelector("body");
+      this.init(container);
+
+      this._responseRequest = {};
+      this.request();
    }
 
-   xhr(page) {
-      this._url.searchParams.set("page", page);
+   init(container) { 
 
+      this._list = document.createElement("ul");
+      container.appendChild(this._list);
+
+      let nav = document.createElement("nav");
+      container.appendChild(nav);
+
+      this._prev = document.createElement("button");
+      this._prev.innerText = "PREV";
+      nav.appendChild(this._prev);
+
+      this._page = document.createElement("span");
+      this._page.innerText = "1";
+      nav.appendChild(this._page);
+
+      this._next = document.createElement("button");
+      this._next.innerText = "NEXT";
+      nav.appendChild(this._next);
+   }
+
+   request() {      
       const xhr = new XMLHttpRequest();
-      xhr.timeout = 10000;
+
       xhr.open("GET", this._url);
       xhr.send();
 
       xhr.onload = function () {
-         this._names = [];
          if (xhr.status !== 200) {
             console.error(`${xhr.status}: ${xhr.statusText}`);
          } else {
             try {
-               this._names = JSON.parse(xhr.response).results.map(elem => elem.name);
+               this._responseRequest = JSON.parse(xhr.response);
             }
             catch (err) {
                console.error(err);
             }
+            this.render();
          }
       }.bind(this);
 
       xhr.onerror = function () {
          console.error("Request is failed!");
-         this._names = [];
       }
    }
 
    render() { 
-      console.log(this._names);
+      this._responseRequest.results
+         .map(elem => elem.name)
+         .forEach(name => {
+            let li = document.createElement("li");
+            li.innerText = name;
+            this._list.appendChild(li);
+         });
    }
 }
 
 const rickandmortyCharacter = new RequestList({
-   url: "https://rickandmortyapi.com/api/character/"
+   url: "https://rickandmortyapi.com/api/character/",
+   container: ".container",
 });
