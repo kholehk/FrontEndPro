59b1e0ef81a78a0709c36f29b4b68c66f9527310
kholehk <66831553+kholehk@users.noreply.github.com>
2020-10-24 19:07:06 +0300

    Bot extends Chat

--- lab20/khoroshykh.lab20.js
+++ lab20/khoroshykh.lab20.js
@@ -89,20 +89,142 @@ class List extends Element {
    }
 }
 
-class Bot {
-   constructor(word) {
-      this._postsForAnswer = [];
-      this._listAnswers = ["Hi", "How are you?", "I'm fine", "Weather is ugly, today", "Bla-bla-bla-...", "Thats all, wolks", "Bye"];
-      this._stopWord = word;
-      this._stopBot = false;
+// class Bot {
+//    constructor(word) {
+//       this._postsForAnswer = [];
+//       this._listAnswers = ["Hi", "How are you?", "I'm fine", "Weather is ugly, today", "Bla-bla-bla-...", "Thats all, wolks", "Bye"];
+//       this._stopWord = word;
+//       this._stopBot = false;
+//    }
+
+//    addPost(message) {
+//       this._postsForAnswer.push(message);
+//    }
+
+//    sendPost() { 
+//       if (this._stopBot) return this._stopWord;
+
+//       const idx = Bot.randomFromRange(this._listAnswers.length - 1);
+
+//       return this._listAnswers[idx];
+//    }
+   
+//    async listenChat() { 
+//       return new Promise((result, reject) => {
+
+//          if (this._stopBot) {
+
+//             reject(new ChatError(0, "Bot is stoped"));
+
+//          } else if (!this._postsForAnswer.length) {
+            
+//             reject(new ChatError(1, "Bot is wait"));
+
+//          } else {
+
+//             this._stopBot = this._postsForAnswer[0] === this._stopWord;
+//             this._postsForAnswer = this._postsForAnswer.slice(1);
+
+//             result(this.sendPost());
+//          }
+//       });
+//    }
+
+//    static async wait(delay) {
+//       return new Promise(result => setTimeout(result, delay));
+//    }
+
+//    static randomFromRange(...arg) {
+//       const [first, last] = arg.length === 1 ? [0, arg[0]] : arg;
+
+//       const random = Math.round(Math.random() * last * 10) % last;
+//       return random < first ? first : random;
+//    }
+// }
+
+// class ChatError extends Error { 
+//    constructor(err, msg) { 
+//       super(msg);
+//       this._code = err;
+//    }
+// }
+
+class Chat extends List {
+   constructor(container, stopMessage) {
+      super("chat");
+
+      if (!container && !(container instanceof HTMLElement)) return;
+
+      const form = new Form((event) => this.sendPost(event));
+
+      [
+         this,
+         form,
+         new Comment(`* Please, say "${stopMessage}", for stop this Bot.`),
+      ]
+         .forEach(obj => container.appendChild(obj.element));
+      
+      form.element.firstElementChild.focus();
+
+      // this._myBot = new Bot(stopWord);
+      // this._listner = setInterval(() => this.listen(), 0);
    }
 
-   addPost(message) {
+   addPost(message, style) {
+      if (!message) return;
+
+      const post = new Post(message, style);
+      this.add(post.element);
+
+      this.element.scrollTo(0, this.element.scrollHeight);
+   }
+
+   sendPost(event) {
+      event.preventDefault();
+
+      const input = event.target.querySelector("input");
+
+      this.addPost(input.value, typePost.user);
+      // this._myBot.addPost(input.value);
+
+      input.value = "";
+      input.focus();
+   }
+}
+
+class Bot extends Chat {
+   constructor(container, stopMessage) {
+      super(container, stopMessage);
+      
+      this._postsForAnswer = [];
+      this._listAnswers = ["Hi", "How are you?", "I'm fine", "Weather is ugly, today", "Bla-bla-bla-...", "Thats all, wolks", "Bye"];
+      this._stopMsg = stopMessage;
+      this._stopBot = false;
+
+      this._listner = setInterval(async () => {
+         try { 
+            const msg = await this.listenChat();
+
+            await Bot.wait(Bot.randomFromRange(1000, 3000));
+            super.addPost(msg, typePost.bot);
+
+         } catch (err) { 
+            if (!err._code && this._listner) { 
+               clearInterval(this._listner);
+               this._listner = null;
+            }
+         }
+      }, 0);
+   }
+
+   addPost(message, style) {
+      super.addPost(message, style);
       this._postsForAnswer.push(message);
    }
 
-   sendPost() { 
-      if (this._stopBot) return this._stopWord;
+   answerPost() {
+
+      if (this._stopBot) return this._stopMsg;
 
       const idx = Bot.randomFromRange(this._listAnswers.length - 1);
 
@@ -122,10 +244,10 @@ class Bot {
 
          } else {
 
-            this._stopBot = this._postsForAnswer[0] === this._stopWord;
+            this._stopBot = this._postsForAnswer[0] === this._stopMsg;
             this._postsForAnswer = this._postsForAnswer.slice(1);
 
-            result(this.sendPost());
+            result(this.answerPost());
          }
       });
    }
@@ -149,64 +271,7 @@ class ChatError extends Error {
    }
 }
 
-class Chat extends List {
-   constructor(container, stopWord) {
-      super("chat");
-
-      if (!container && !(container instanceof HTMLElement)) return;
-
-      const form = new Form((event) => this.sendPost(event));
-
-      [
-         this,
-         form,
-         new Comment(`* Please, say "${stopWord}", for stop this Bot.`),
-      ]
-         .forEach(obj => container.appendChild(obj.element));
-      
-      form.element.firstElementChild.focus();
-
-      this._myBot = new Bot(stopWord);
-      this._listner = setInterval(() => this.listen(), 0);
-   }
-
-   addPost(message, style) {
-      if (!message) return;
-
-      const post = new Post(message, style);
-      this.add(post.element);
-
-      this.element.scrollTo(0, this.element.scrollHeight);
-   }
-
-   sendPost(event) {
-      event.preventDefault();
-
-      const input = event.target.querySelector("input");
-
-      this.addPost(input.value, typePost.user);
-      this._myBot.addPost(input.value);
-
-      input.value = "";
-      input.focus();
-   }
-
-   async listen() {
-      try { 
-         const msg = await this._myBot.listenChat();
-
-         await Bot.wait(Bot.randomFromRange(1000, 3000));
-         this.addPost(msg, typePost.bot);
-
-      } catch (err) { 
-         if (!err._code && this._listner) { 
-            clearInterval(this._listner);
-            this._listner = null;
-         }
-      }
-   }
-}
-
 const container = document.querySelector(".container");
 
-const myChatBot = new Chat(container, "Bye");
+// const myChatBot = new Chat(container, "Bye");
+const myChatBot = new Bot(container, "Bye");
