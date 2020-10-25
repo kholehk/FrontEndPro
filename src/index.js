305392f45fc4871230d1f5422310e65eaf6e7eff
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2020-10-25 23:32:36 +0200

    date-fns

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 "use strict";
+
+import { format } from 'date-fns'
+
+const date = format(new Date(), 'iiii');
+
+console.log("Hello, world!", date);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
    entry: './src/index.js',
    output: {
-      filename: 'main.js',
+      filename: 'main.[hash].js',
       path: path.resolve(__dirname, 'dist'),
    },
+   plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin(),
+   ],
 };
